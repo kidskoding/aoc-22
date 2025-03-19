feab5836cfcb93d922fccb73671f65f44ccb02b6
Anirudh Konidala <kidskodingclub@gmail.com>
2025-03-19 15:20:44 -0500

    2.2 done

--- src/rock_paper_scissors.hpp
+++ src/rock_paper_scissors.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <fstream>
+#include <iostream>
 #include <unordered_map>
 
 int getScore() {
@@ -34,5 +35,48 @@ int getScore() {
         }
     }
 
+    return score;
+}
+
+int getScore2() {
+    std::fstream fs("./input/rock_paper_scissors.txt");
+
+    std::unordered_map<char, int> scores;
+    scores.insert(std::make_pair('X', 1));
+    scores.insert(std::make_pair('Y', 2));
+    scores.insert(std::make_pair('Z', 3));
+
+    std::unordered_map<char, char> outcomes;
+    outcomes.insert(std::make_pair('A', 'Y'));
+    outcomes.insert(std::make_pair('B', 'Z'));
+    outcomes.insert(std::make_pair('C', 'X'));
+
+    std::unordered_map<char, char> draws;
+    draws.insert(std::make_pair('A', 'X'));
+    draws.insert(std::make_pair('B', 'Y'));
+    draws.insert(std::make_pair('C', 'Z'));
+
+    std::unordered_map<char, char> losses;
+    losses.insert(std::make_pair('A', 'Z'));
+    losses.insert(std::make_pair('B', 'X'));
+    losses.insert(std::make_pair('C', 'Y'));
+
+    int score = 0;
+    while(fs.good()) {
+        std::string input;
+        std::getline(fs, input);
+        if(input.empty()) continue;
+
+        char opponent = input[0];
+        char desired = input[2];
+
+        if(desired == 'X') { // lose
+            score += scores[losses[opponent]];
+        } else if(desired == 'Y') { // draw
+            score += scores[draws[opponent]] + 3;
+        } else { // win
+            score += scores[outcomes[opponent]] + 6;
+        }
+    }
     return score;
 }
